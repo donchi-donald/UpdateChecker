03c25c8de053c68c332a477afb2f7dd9323cc80a
donchi-donald <donchidonald2000@gmail.com>
2023-02-22 07:48:38 +0100

    Aufrufen der Hauptmethode in der Main-Methode (closes #6)

--- src/main/java/de/awp_consult/App.java
+++ src/main/java/de/awp_consult/App.java
@@ -1,13 +1,19 @@
 package de.awp_consult;
 
+import de.awp_consult.util.Util;
+import de.awp_consult.util.UtilImpl;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-
+    public static void main( String[] args ) throws IOException, NoSuchAlgorithmException {
+        Util util = new UtilImpl();
+        util.downloadAndUpdateFiles();
     }
 }
