1af0a47ab79d7f98b4520c39331436b8dff5f936
donchi-donald <donchidonald2000@gmail.com>
2023-02-22 07:32:34 +0100

    testDownloadFile done (#5)

--- src/test/java/de/awp_consult/util/UtilImplTest.java
+++ src/test/java/de/awp_consult/util/UtilImplTest.java
@@ -1,10 +1,26 @@
 package de.awp_consult.util;
 
+import de.awp_consult.config.Config;
 import junit.framework.TestCase;
+import org.junit.Before;
+
+import java.io.IOException;
 
 public class UtilImplTest extends TestCase {
 
-    public void testDownloadFile() {
+    private  Util util;
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        util =  new UtilImpl();
+
+    }
+
+    public void testDownloadFile() throws IOException {
+        byte[] bytes = util.downloadFile(Config.FIREFOX_DOWNLOAD_URL);
+        assertTrue("FirefoxDownload not work",bytes.length>0);
+        byte[] bytes2 = util.downloadFile(Config.TOTAL_COMMANDER_DOWNLOAD_URL);
+        assertTrue("Total commander Download not work",bytes2.length>0);
     }
 
     public void testGetChecksum() {
